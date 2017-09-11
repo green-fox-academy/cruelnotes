56dc9fd951d6f3ff679a532b9511f51fcba1252a
cruelnotes <adam.sequence@gmail.com>
2017-09-11 16:20:04 +0200

    Added FizzBuzz

--- week-02/day-01/src/FizzBuzz.java
+++ week-02/day-01/src/FizzBuzz.java
@@ -3,4 +3,32 @@ public class FizzBuzz {
 // But for multiples of three print “Fizz” instead of the number
 // and for the multiples of five print “Buzz”.
 // For numbers which are multiples of both three and five print “FizzBuzz”.
+
+    public static void main(String[] args) {
+
+        int count = 1;
+        while (count < 101) {
+
+            if (count % 3 == 0 && count % 5 != 0) {
+                System.out.println("Fizz");
+                count++;
+            } else if (count % 5 == 0 && count % 3 != 0) {
+                System.out.println("Buzz");
+                count++;
+            } else if (count % 5 == 0 && count % 3 == 0) {
+                System.out.println("FizzBuzz");
+                count++;
+            } else {
+                System.out.println(count);
+                count++;
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
