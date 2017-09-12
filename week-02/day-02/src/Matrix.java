69b158a8c1bc73bd6dd5cea1f4948613e2ddbbf3
cruelnotes <adam.sequence@gmail.com>
2017-09-12 16:15:43 +0200

    Matrix

--- week-02/day-02/src/Matrix.java
+++ week-02/day-02/src/Matrix.java
@@ -8,4 +8,35 @@ public class Matrix {
     //   0 0 0 1
     //
     // - Print this two dimensional array to the output
+    public static void main(String[] args) {
+
+        int row = 4;
+        int col = 4;
+
+        int[][] matrix = new int[row][col];
+
+        for (int i = 0; i < row-1; i++) {
+
+            for (int k = 0; k < col-1; k++) {
+
+                if (i == k) {
+                    matrix[i][k] = 1;
+                } else {
+                    matrix[i][k] = 0;
+                }
+
+            }
+
+
+        }
+
+        for (int i = 0; i <= row-1; i++) {
+            System.out.println("\r\n");
+            for (int j = 0; j <= col-1; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+        }
+
+    }
+
 }
