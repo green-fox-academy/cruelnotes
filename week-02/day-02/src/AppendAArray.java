c6d50c42bcec565a77ab067d1125512cffd302f0
cruelnotes <adam.sequence@gmail.com>
2017-09-12 15:23:05 +0200

    AppendAArray

---
+++ week-02/day-02/src/AppendAArray.java
@@ -0,0 +1,18 @@
+public class AppendAArray {
+
+    // - Create an array variable named `nimals`
+    //   with the following content: `["kuty", "macs", "cic"]`
+    // - Add all elements an `"a"` at the end
+
+    public static void main(String[] args) {
+
+        String[] nimals = {"kuty", "macs", "cic"};
+        //    System.out.println(appendA(nimals));
+
+        for (int i = 0; i < nimals.length; i++) {
+            nimals[i] = nimals[i] + "a";
+
+
+        }
+    }
+}
