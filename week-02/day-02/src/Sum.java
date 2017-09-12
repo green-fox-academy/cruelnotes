7ab52f5bb6a26ec95f1f33926ce566934de46314
cruelnotes <adam.sequence@gmail.com>
2017-09-12 14:07:25 +0200

    Sum

---
+++ week-02/day-02/src/Sum.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Sum {
+    //  Create the usual class wrapper
+//  and main method on your own.
+
+// - Write a function called `sum` that sum all the numbers
+//   until the given parameter
+
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Input a number until when the program should sum the numbers.");
+        int sumUntil = userInput.nextInt();
+
+        System.out.println(sum(sumUntil));
+
+    }
+
+    public static int sum(int top) {
+        int container = 0;
+            for (int i = 0; i <=  top; i++) {
+                container += i;
+
+            }
+        return container;
+    }
+}
