689d02b258236fa0e5060dc6ec288138a67ceb3f
cruelnotes <adam.sequence@gmail.com>
2017-09-12 15:54:08 +0200

    Reverse list

---
+++ week-02/day-02/src/ReverseList.java
@@ -0,0 +1,22 @@
+public class ReverseList {
+
+    public static void main(String[] args) {
+
+        int[] aj = {3, 4, 5, 6, 7};
+
+        int[] reverse = new int[aj.length];
+
+
+        for (int i = aj.length - 1 , k = 0; i >= 0; i--, k++) {
+            reverse[k] = aj[i];
+        }
+
+        for (int j = 0; j < aj.length; j++) {
+
+            System.out.println(reverse[j]);
+
+        }
+
+    }
+
+}
