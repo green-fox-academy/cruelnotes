8cf2b69cbb6fd9b762c84c4c5b67530b788d5a7f
cruelnotes <adam.sequence@gmail.com>
2017-09-12 13:42:35 +0200

    .gitignore edited

---
+++ week-02/day-02/src/Doubling.java
@@ -0,0 +1,15 @@
+public class Doubling {
+    public static void main(String[] args) {
+// - Create an integer variable named `ak` and assign the value `123` to it
+// - Create a function called `doubling` that doubles it's input parameter
+// - Print the result of `doubling(ak)`
+
+
+        int ak = 123;
+        doubling(ak);
+    }
+
+    public static void doubling(int x) {
+        System.out.println(2*x);
+    }
+}
