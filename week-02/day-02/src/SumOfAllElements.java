3bdae88c80129ae4d0953bf15dd853757b9ccda2
cruelnotes <adam.sequence@gmail.com>
2017-09-12 15:36:29 +0200

    SumOfAllElements

---
+++ week-02/day-02/src/SumOfAllElements.java
@@ -0,0 +1,23 @@
+public class SumOfAllElements {
+
+    // - Create an array variable named `ai`
+    //   with the following content: `[3, 4, 5, 6, 7]`
+    // - Print the sum of the elements in `ai`
+
+    public static void main(String[] args) {
+
+    int[] ai = {3, 4, 5, 6, 7};
+
+        System.out.println(sumOfAll(ai));
+
+    }
+
+    public static int sumOfAll(int[] elements) {
+
+        int total = 0;
+            for (int i = 0; i < elements.length; i++) {
+                total += elements[i];
+            }
+        return total;
+    }
+}
