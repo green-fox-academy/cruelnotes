378690500868b89afcc3778514597ecd6fca7340
cruelnotes <adam.sequence@gmail.com>
2017-09-14 19:52:26 +0200

    1 hour

--- sandbox/battleship/src/com/stringtutorial/Main.java
+++ sandbox/battleship/src/com/stringtutorial/Main.java
@@ -7,17 +7,14 @@ public class Main {
 
     public static void main(String[] args) {
         int TABLE_SIZE = 4;
-
-
         printTable(TABLE_SIZE);
-        int[][] tableArray = new int[TABLE_SIZE][TABLE_SIZE];
-        int rowNr = randInt(0,TABLE_SIZE);
-        int colNr = randInt(0,TABLE_SIZE);;
 
-
-
+        generateShips(TABLE_SIZE, 3);
 
     }
+
+
+
 
     public static int randInt(int min, int max) {
         Random rand = new Random();
@@ -25,13 +22,40 @@ public class Main {
         return randomNum;
     }
 
-    static void printTable(int size) {
+    static void generateShips(int size, int shipNr) {
+        String[][] tableArray = new String[size][size];
+
+        for (int i = 0; i < shipNr ; i++) {
+            int rowNr = randInt(0,size);
+            int colNr = randInt(0,size);
+            if (tableArray[rowNr][colNr] != "X") {
+                tableArray[rowNr][colNr] = "X";
+            } else {
+                i--;
+            }
+
+            for(int x = 0; x < 4; x++){
+                System.out.println();
+                for(int y = 0; y < 4; y++){
+                    System.out.print(tableArray[x][y]);
+                }
+            }
+
+
+        }
+
+    }
+
+    public static void printTable(int size) {
         for (int i = 0; i <= size; i++) {
             System.out.println();
             for (int j = 0; j <= 4 ; j++) {
                 System.out.print("O ");
             }
         }
+
+
+
 
     }
 
