bb6bfff7395aa7d0b89f3f2e17ac17bfb76cff3c
cruelnotes <adam.sequence@gmail.com>
2017-09-15 14:27:32 +0200

    palindromeSearcher

---
+++ week-02/day-05/src/palindromeSearcher.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class palindromeSearcher {
+
+
+    public static void main(String[] args) {
+
+    Scanner anagramScanner = new Scanner(System.in);
+
+    System.out.println("This program will search within the given words if there is a palindrome to return");
+
+    System.out.println("Please provide a line to search for palindromes:");
+    String inputStringLine = anagramScanner.nextLine();
+
+    System.out.println(searchPalindrome(inputStringLine));
+    }
+
+    private static String searchPalindrome(String inputStringLine) {
+        ArrayList<String> palidromeArrayList = new ArrayList<String>();
+
+        for (int i = 0; i < inputStringLine.length() - 3; i++) {
+            for (int j = i + 3; j < inputStringLine.length(); j++) {
+                String wordSubstring = inputStringLine.substring(i,j);
+                String substringReverse = new StringBuffer(wordSubstring).reverse().toString();
+                if (wordSubstring.equals(substringReverse)) {
+                    palidromeArrayList.add(wordSubstring);
+                }
+            }
+        }
+        String palindrome = String.join(", ", palidromeArrayList);
+        return palindrome;
+    }
+}
