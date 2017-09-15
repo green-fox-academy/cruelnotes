eea3164d5789d85837665b5b67645f8f92253b6d
cruelnotes <adam.sequence@gmail.com>
2017-09-15 09:51:14 +0200

    init board

--- sandbox/battleship/src/battleship2.java
+++ sandbox/battleship/src/battleship2.java
@@ -2,4 +2,22 @@
 
 public class battleship2 {
 
+
+    public static void main(String[] args) {
+
+        int[][] table = new int[5][5];
+        initTable(table);
+
+
+    }
+
+
+    public static void initTable(int[][] board){
+        for(int row=0 ; row < 5 ; row++ )
+            for(int col=0 ; col < 5 ; col++ )
+                board[row][col]=-1;
+    }
+
+
+
 }
--- sandbox/battleship/src/com/stringtutorial/Main.java
+++ sandbox/battleship/src/com/stringtutorial/Main.java
@@ -1,65 +1,65 @@
-package com.stringtutorial;
-
-import java.util.Random;
-import java.util.Scanner;
-
-public class Main {
-
-    public static void main(String[] args) {
-        int TABLE_SIZE = 4;
-        printTable(TABLE_SIZE);
-
-        generateShips(TABLE_SIZE, 3);
-
-    }
-
-
-
-
-    static int randInt(int min, int max) {
-        Random rand = new Random();
-        int randomNum = rand.nextInt((max - min) + 1) + min;
-        return randomNum;
-    }
-
-    static void generateShips(int size, int shipNr) {
-        String[][] tableArray = new String[size][size];
-
-        for (int i = 0; i < shipNr ; i++) {
-            int rowNr = randInt(0,size);
-            int colNr = randInt(0,size);
-
-            tableArray[r1][r2]
-
-            if (tableArray[rowNr][colNr] == "") {
-                tableArray[rowNr][colNr] = "X ";
-            } else {
-                i--;
-            }
-
-            for(int x = 0; x < 4; x++){
-                System.out.println();
-                for(int y = 0; y < 4; y++){
-                    System.out.print(tableArray[x][y]);
-                }
-            }
-
-
-        }
-
-    }
-
-    static void printTable(int size) {
-        for (int i = 0; i <= size; i++) {
-            System.out.println();
-            for (int j = 0; j <= 4 ; j++) {
-                System.out.print("O ");
-            }
-        }
-
-
-
-
-    }
-
-}
+//package com.stringtutorial;
+//
+//import java.util.Random;
+//import java.util.Scanner;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        int TABLE_SIZE = 4;
+//        printTable(TABLE_SIZE);
+//
+//        generateShips(TABLE_SIZE, 3);
+//
+//    }
+//
+//
+//
+//
+//    static int randInt(int min, int max) {
+//        Random rand = new Random();
+//        int randomNum = rand.nextInt((max - min) + 1) + min;
+//        return randomNum;
+//    }
+//
+//    static void generateShips(int size, int shipNr) {
+//        String[][] tableArray = new String[size][size];
+//
+//        for (int i = 0; i < shipNr ; i++) {
+//            int rowNr = randInt(0,size);
+//            int colNr = randInt(0,size);
+//
+//            tableArray[r1][r2]
+//
+//            if (tableArray[rowNr][colNr] == "") {
+//                tableArray[rowNr][colNr] = "X ";
+//            } else {
+//                i--;
+//            }
+//
+//            for(int x = 0; x < 4; x++){
+//                System.out.println();
+//                for(int y = 0; y < 4; y++){
+//                    System.out.print(tableArray[x][y]);
+//                }
+//            }
+//
+//
+//        }
+//
+//    }
+//
+//    static void printTable(int size) {
+//        for (int i = 0; i <= size; i++) {
+//            System.out.println();
+//            for (int j = 0; j <= 4 ; j++) {
+//                System.out.print("O ");
+//            }
+//        }
+//
+//
+//
+//
+//    }
+//
+//}
