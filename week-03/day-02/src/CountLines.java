cc579483b9898a58c22ce4cb1a17185dcce1d0bd
cruelnotes <adam.sequence@gmail.com>
2017-09-19 14:50:01 +0200

    CountLines

--- week-03/day-02/src/CountLines.java
+++ week-03/day-02/src/CountLines.java
@@ -1,4 +1,39 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CountLines {
+
 // Write a function that takes a filename as string,
 // then returns the number of lines the file contains.
 // It should return zero if it can't open the file, and
 // should not raise any error.
+
+    public static void main(String[] args) {
+
+        String src = "src/duplicated-chars.txt";
+        lineCountCall(src);
+    }
+
+    public static int lineCountCall(String src) {
+        int total = 0;
+
+        Path fileSource = Paths.get(src);
+        List<String> lines = null;
+        try {
+            lines = Files.readAllLines(fileSource);
+
+        } catch (IOException e) {
+            //  System.out.println("0");
+        }
+        for (String line: lines) {
+            total += 1;
+        }
+        System.out.println(total);
+        return total;
+    }
+
+
+}
