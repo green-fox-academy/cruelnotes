beb68b017d2d2b16bc2184ae31b919c363bbbad2
cruelnotes <adam.sequence@gmail.com>
2017-09-19 11:10:52 +0200

    DivideByZero userinput and method init

--- week-03/day-02/src/DivideByZero.java
+++ week-03/day-02/src/DivideByZero.java
@@ -1,4 +1,24 @@
+import java.util.Scanner;
+
+public class WriteSingleLine {
 // create a function that takes a number
 // divides ten with it,
 // and prints the result.
 // it should print "fail" if the parameter is 0
+
+
+    public static void main(String[] args) {
+
+        Scanner intScanner = new Scanner(System.in);
+        System.out.println("Please input an integer: ");
+        int userInput =   intScanner.nextInt(System.in);
+
+        System.out.println(divisionByTen(userInput));
+
+
+
+    }
+
+    private static int divisionByTen(int userInput) {
+    }
+}
