02751b5c2e483d9ae71b85eb5a96027af0f39c71
cruelnotes <adam.sequence@gmail.com>
2017-09-19 17:02:38 +0200

    WriteMultiplyLines, Copyfile init

--- week-03/day-02/src/CopyFile.java
+++ week-03/day-02/src/CopyFile.java
@@ -1,3 +1,19 @@
+
+
+public class CopyFile {
 // Write a function that copies a file to an other
 // It should take the filenames as parameters
 // It should return a boolean that shows if the copy was successful
+
+    public static void main(String[] args) {
+
+        copy("source.txt", "dest.txt");
+
+    }
+
+    public static void copy(String sourcePath, String destinationPath) throws IOException {
+        Files.copy(Paths.get(sourcePath), new FileOutputStream(destinationPath));
+    }
+
+}
+
--- week-03/day-02/src/WriteMultipleLines.java
+++ week-03/day-02/src/WriteMultipleLines.java
@@ -1,3 +1,13 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WriteMultipleLines {
+
 // Create a function that takes 3 parameters: a path, a word and a number,
 // than it should write to a file.
 // The path parameter should be a string, that describes the location of the file.
@@ -6,3 +16,34 @@
 // So if the word is "apple" and the number is 5, than it should write 5 lines
 // to the file and each line should be "apple"
 // The function should not raise any error if it could not write the file.
+
+
+public static void main(String[] args) {
+
+        int num = 10;
+        String word = "a string or two";
+        String src = "src/my-files.txt";
+        writeMultiLines(src, word, num);
+
+
+    }
+
+    private static void writeMultiLines(String src, String word, int num) {
+
+        Path fileSource = Paths.get(src);
+
+        List<String> content = new ArrayList();
+        content.add(word);
+
+        try {
+            for (int i = 0; i <= num ; i++) {
+                Files.write(fileSource, content, StandardOpenOption.APPEND);
+            }
+
+        } catch (IOException e) {
+        //    System.out.println("Unable to read file: my-file.txt");
+        }
+
+
+    }
+}
