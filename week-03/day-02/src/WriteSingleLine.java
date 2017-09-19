d70dd3cd89a904c6f0e87ff6a39e070855f0c85e
cruelnotes <adam.sequence@gmail.com>
2017-09-19 15:02:59 +0200

    WriteSingleLine

--- week-03/day-02/src/WriteSingleLine.java
+++ week-03/day-02/src/WriteSingleLine.java
@@ -1,16 +1,30 @@
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WriteSingleLine {
 
-  public static void main(String[] args) {
     // Open a file called "my-file.txt"
     // Write your name in it as a single line
     // If the program is unable to write the file,
     // then it should print an error message like: "Unable to write file: my-file.txt"
 
+  public static void main(String[] args) {
 
+      Path fileSource = Paths.get("src/my-file.txt");
+
+      List<String> content = new ArrayList();
+      content.add("your name in it as a single line");
+
+      try {
+        Files.write(fileSource, content);
+
+      } catch (IOException e) {
+        System.out.println("Unable to read file: my-file.txt");
+      }
   }
 }
 
