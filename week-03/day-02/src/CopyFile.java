a440e5aa200467192414ca95847a24a444e76c20
cruelnotes <adam.sequence@gmail.com>
2017-09-19 17:19:58 +0200

    Copyfile

--- week-03/day-02/src/CopyFile.java
+++ week-03/day-02/src/CopyFile.java
@@ -1,19 +1,30 @@
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class CopyFile {
-// Write a function that copies a file to an other
-// It should take the filenames as parameters
-// It should return a boolean that shows if the copy was successful
+
+    // Write a function that copies a file to an other
+    // It should take the filenames as parameters
+    // It should return a boolean that shows if the copy was successful
 
     public static void main(String[] args) {
-
-        copy("source.txt", "dest.txt");
-
+        System.out.println(copyFiles("src/my-file.txt", "src/my-fileCopy.txt"));
     }
 
-    public static void copy(String sourcePath, String destinationPath) throws IOException {
-        Files.copy(Paths.get(sourcePath), new FileOutputStream(destinationPath));
+    public static boolean copyFiles (String inputSrc, String outputSrc) {
+        Path filepath = Paths.get(inputSrc);
+        Path destFileContent = Paths.get(outputSrc);
+        boolean success;
+        try {
+            List<String> sourceFileContent = Files.readAllLines(filepath);
+            Files.write(destFileContent, sourceFileContent);
+            success = true;
+        } catch (IOException e){
+            success = false;
+        }
+        return success;
     }
-
 }
-
