a52322ed2ae8a5717844edb5d4b4872eee323a44
cruelnotes <adam.sequence@gmail.com>
2017-09-20 10:24:17 +0200

    Ex.es init

--- week-03/day-02/src/Doubled.java
+++ week-03/day-02/src/Doubled.java
@@ -1,15 +1,10 @@
-import java.util.Scanner;
 
 public class Doubled {
 
     public static void main(String[] args) {
         // Create a method that decrypts the duplicated-chars.txt
 
-        System.out.println();
-
-        Scanner myScan = new Scanner(System.in);
 
     }
 }
 
-//ctrl+j
