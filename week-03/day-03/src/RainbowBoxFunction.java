9aa7c3193abc915f1a16af58a6daf96169ea74e4
cruelnotes <adam.sequence@gmail.com>
2017-09-20 15:40:54 +0200

    RainbowBox not finished

--- week-03/day-03/src/RainbowBoxFunction.java
+++ week-03/day-03/src/RainbowBoxFunction.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 
 import java.awt.*;
+import java.util.Random;
+import java.util.Scanner;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
@@ -12,11 +14,32 @@ public class RainbowBoxFunction {
       // and draws a square of that size and color to the center of the canvas.
       // create a loop that fills the canvas with rainbow colored squares.
 
+        int random255 = (int)(Math.random()*256);
+
+        Scanner drawScanner = new Scanner(System.in);
+        System.out.println("Size of the box please: ");
+        int boxSize = drawScanner.nextInt();
+        Color inputColor = new Color(100, 100, 100);
+
+        drawMiddleBox(graphics, boxSize, inputColor);
 
 
     }
 
-//    Don't touch the code below
+    private static void drawMiddleBox(Graphics g, int size, Color color) {
+        int random255 = (int)(Math.random()*256);
+        g.setColor(color);
+        g.fillRect(WIDTH / 2 - size / 2,HEIGHT / 2 - size / 2, size, size);
+
+        for (int i = size; i < WIDTH ; i+=25) {
+            g.setColor(new Color(random255, random255, random255));
+            g.fillRect(WIDTH / 2 - size / 2,HEIGHT / 2 - size / 2, i, i);
+        }
+
+
+    }
+
+    //    Don't touch the code below
     static int WIDTH = 320;
     static int HEIGHT = 343;
 
