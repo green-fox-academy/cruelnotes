e90e60893b901e0a5c47de2a1a4bedb1c4876907
cruelnotes <adam.sequence@gmail.com>
2017-09-20 11:04:02 +0200

    ColoredBox

--- week-03/day-03/src/ColoredBox.java
+++ week-03/day-03/src/ColoredBox.java
@@ -9,7 +9,23 @@ public class ColoredBox {
     public static void mainDraw(Graphics graphics){
       // draw a box that has different colored lines on each edge.
 
+        int a1 =10 ;
+        int a2 =10 ;
+        int b1 =110 ;
+        int b2 =10 ;
+        int c1 =110 ;
+        int c2 =110 ;
+        int d1 =10 ;
+        int d2 =110 ;
 
+        graphics.setColor(Color.RED);
+        graphics.drawLine(a1, a2, b1, b2);
+        graphics.setColor(Color.ORANGE);
+        graphics.drawLine(b1, b2, c1, c2);
+        graphics.setColor(Color.CYAN);
+        graphics.drawLine(c1, c2, d1, d2);
+        graphics.setColor(Color.WHITE);
+        graphics.drawLine(d1, d2, a1, a2);
 
     }
 
