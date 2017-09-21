0c4beefcf607799aa420a816e9c1f3d8817314d8
cruelnotes <adam.sequence@gmail.com>
2017-09-21 12:53:17 +0200

    String1

--- week-03/day-04/src/String1.java
+++ week-03/day-04/src/String1.java
@@ -1,2 +1,16 @@
 // Given a string, compute recursively (no loops) a new string where all the
 // lowercase 'x' chars have been changed to 'y' chars.
+
+public class String1 {
+    public static void main(String[] args) {
+        System.out.println(changeX("isx itx working?X" ));
+    }
+
+    private static String changeX(String s) {
+        if (s.contains("x")) {
+            return changeX(s.replaceFirst("x", "y"));
+        } else {
+            return s;
+        }
+    }
+}
