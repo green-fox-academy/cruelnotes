941a62a39be8f063b711c4bc64727d69eab70e54
cruelnotes <adam.sequence@gmail.com>
2017-09-21 12:28:45 +0200

    Bunny2

--- week-03/day-04/src/Bunny2.java
+++ week-03/day-04/src/Bunny2.java
@@ -2,3 +2,21 @@
 // (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
 // have 3 ears, because they each have a raised foot. Recursively return the
 // number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+public class Bunny2 {
+    public static void main(String[] args) {
+        System.out.println(floppyEars(3, 0));
+    }
+
+    private static int floppyEars(int n, int total) {
+        if (n == 0) {
+            return total;
+        } else if (n % 2 != 0)  {
+            total = total + 2;
+            return floppyEars(n - 1, total);
+        } else {
+            total = total + 3;
+            return floppyEars(n - 1, total);
+        }
+    }
+}
