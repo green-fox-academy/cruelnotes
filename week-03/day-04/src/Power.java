c0781f5a3cc0d9cee28bc7091cec33a1801617ab
cruelnotes <adam.sequence@gmail.com>
2017-09-21 12:14:04 +0200

    Power

--- week-03/day-04/src/Power.java
+++ week-03/day-04/src/Power.java
@@ -1,2 +1,18 @@
 // Given base and n that are both 1 or more, compute recursively (no loops)
 // the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+
+public class Power {
+    public static void main(String[] args) {
+        System.out.println(callPowerOf(3, 2, 1));
+    }
+
+    private static int callPowerOf(int base, int power, int total) {
+        if (power == 0) {
+            return total;
+        } else {
+            total = base * total;
+            power -= 1;
+            return callPowerOf(base, power, total);
+        }
+    }
+}
