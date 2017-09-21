a8762cdb508a739cbc24a3163217cc477dff1f0e
cruelnotes <adam.sequence@gmail.com>
2017-09-21 12:55:39 +0200

    String2

--- week-03/day-04/src/String2.java
+++ week-03/day-04/src/String2.java
@@ -1,1 +1,17 @@
 // Given a string, compute recursively a new string where all the 'x' chars have been removed.
+
+public class String2 {
+    public static void main(String[] args) {
+        System.out.println(changeX("isx itx working?X" ));
+    }
+
+    private static String changeX(String s) {
+        if (s.contains("x")) {
+            return changeX(s.replaceFirst("x", ""));
+        } else if (s.contains("X")) {
+            return changeX(s.replaceFirst("X", ""));
+        } else {
+            return s;
+        }
+    }
+}
