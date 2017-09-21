65a1fe3fc1d8949c1b7830b3df4d7f9fde448537
cruelnotes <adam.sequence@gmail.com>
2017-09-21 12:23:01 +0200

    Bunny1

--- week-03/day-04/src/Bunny1.java
+++ week-03/day-04/src/Bunny1.java
@@ -1,2 +1,17 @@
 // We have a number of bunnies and each bunny has two big floppy ears.
 // We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+
+public class Bunny1 {
+    public static void main(String[] args) {
+        System.out.println(floppyEars(10, 0));
+    }
+
+    private static int floppyEars(int n, int total) {
+            if (n == 0) {
+                return total;
+            } else {
+                total = total + 2;
+                return floppyEars(n - 1, total);
+        }
+    }
+}
