bc5851d36f1e31762c95c07423e2cf0d7cd0a419
cruelnotes <adam.sequence@gmail.com>
2017-09-27 10:50:43 +0200

    Apples, ApplesTest

---
+++ week-04/day-03/test/ApplesTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ApplesTest {
+
+    String msg = "apples";
+    Apples apples = new Apples();
+
+    public ApplesTest() {
+    }
+
+    @Test
+    public void testSoutApples() {
+        assertEquals(msg, apples.getApple());
+    }
+
+}
