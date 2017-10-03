a093688cd20293a70f75cd3710f69dc6e2a59276
cruelnotes <adam.sequence@gmail.com>
2017-10-03 16:31:34 +0200

    Walls are not just for the eyes anymore.. Our hero gets confused as the world changes around him.

--- week-05/RPG-Game/src/Board.java
+++ week-05/RPG-Game/src/Board.java
@@ -10,6 +10,7 @@ public class Board extends JComponent implements KeyListener {
     int testBoxX;
     int testBoxY;
     String heroImgPos;
+ //   private int[][] boardGame;
 
     public Board() {
         testBoxX = 0;
@@ -31,38 +32,70 @@ public class Board extends JComponent implements KeyListener {
 //        floor.draw(graphics);
 //    }
 
-
-
+        int row,col;
+        int[][] boardGame = new int[][]{
+                {0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0, 1, 0, 1, 1, 0},
+                {0, 1, 1, 1, 0, 1, 0, 1, 1, 0},
+                {0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+                {1, 1, 1, 1, 0, 1, 1, 1, 1, 0},
+                {0, 1, 0, 1, 0, 0, 0, 0, 1, 0},
+                {0, 1, 0, 1, 0, 1, 1, 0, 1, 0},
+                {0, 0, 0, 0, 0, 1, 1, 0, 1, 0},
+                {0, 1, 1, 1, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 1, 0, 1, 1, 0, 1, 0},
+        };
     @Override
     public void paint(Graphics graphics) {
         super.paint(graphics);
-       // graphics.fillRect(testBoxX, testBoxY, 72, 72);
-        // here you have a 720x720 canvas
-        // you can create and draw an image using the class below e.g.
-        ;
+
+        // ---------------------- Board Array ------------------------//
+
+        //------------------------Filling Walls-----------------------//
 
 
-        int row,col;
-        int[][] boardGame = new int[10][10];
-        for(row = 0; row < boardGame.length; row++) {
-            for (col = 0; col < boardGame[row].length; col++) {
-                boardGame[row][col] = 0;
-            }
-        }
+        System.out.println();
+
+        //------------------------Printing boardArray-----------------//
 
         for(row = 0; row < boardGame.length; row++){
             System.out.println();
-            for(col = 0; col < boardGame[row].length; col++){
+            for(col = 0; col < boardGame[row].length; col++) {
                 System.out.print(boardGame[row][col]);
+                if (boardGame[row][col] == 0) {
+                    PositionedImage floor = new PositionedImage("./assets/floor.png", col, row);
+                    floor.draw(graphics);
+                } else if (boardGame[row][col] == 1) {
+                    PositionedImage wall = new PositionedImage("./assets/wall.png", col, row);
+                    wall.draw(graphics);
+                }
             }
         }
 
-        for (int floorPosX = 0; floorPosX < 720 ; floorPosX +=72) {
-            for (int floorPosY = 0; floorPosY < 720 ; floorPosY+=72) {
-                PositionedImage floor = new PositionedImage("./assets/floor.png", floorPosX, floorPosY);
-                floor.draw(graphics);
-            }
-        }
+        //------------------------Filling Floor-----------------------//
+
+//        for (int floorPosX = 0; floorPosX < 720 ; floorPosX +=72) {
+//            for (int floorPosY = 0; floorPosY < 720 ; floorPosY+=72) {
+//                PositionedImage floor = new PositionedImage("./assets/floor.png", floorPosX, floorPosY);
+//                floor.draw(graphics);
+//            }
+//        }
+
+//        for (int titleRow = 0; titleRow < 10 ; titleRow++) {
+//            for (int titleCol = 0; titleCol < 10 ; titleCol++) {
+//                if (boardGame[titleRow][titleCol] == 0) {
+//                PositionedImage floor = new PositionedImage("./assets/floor.png", titleRow, titleCol);
+//                floor.draw(graphics);
+//                } else if (boardGame[titleRow][titleCol] == 1) {
+//                PositionedImage wall = new PositionedImage("./assets/wall.png", titleRow, titleCol);
+//                wall.draw(graphics);
+//
+//                }
+//            }
+//        }
+
+
+
 
         PositionedImage image = new PositionedImage(heroImgPos, testBoxX, testBoxY);
         image.draw(graphics);
@@ -101,16 +134,24 @@ public class Board extends JComponent implements KeyListener {
         // When the up or down keys hit, we change the position of our box
         if (e.getKeyCode() == KeyEvent.VK_UP) {
             heroImgPos = "./assets/hero-up.png";
-            testBoxY -= 72;
+            if (boardGame[testBoxY - 1][testBoxX] == 0) {
+                testBoxY -= 1;
+            }
         } else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
             heroImgPos = "./assets/hero-down.png";
-            testBoxY += 72;
+            if (boardGame[testBoxY + 1][testBoxX] == 0) {
+                testBoxY += 1;
+            }
         } else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
             heroImgPos = "./assets/hero-left.png";
-            testBoxX -= 72;
+            if (boardGame[testBoxY][testBoxX - 1] == 0) {
+                testBoxX -= 1;
+            }
         } else if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
             heroImgPos = "./assets/hero-right.png";
-            testBoxX += 72;
+            if (boardGame[testBoxY][testBoxX + 1] == 0) {
+                testBoxX += 1;
+            }
         }
         // and redraw to have a new picture with the new coordinates
         repaint();
