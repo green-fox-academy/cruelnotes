78ad9030ae546ccd6f565ebae3da66a396b6a9c1
cruelnotes <adam.sequence@gmail.com>
2017-10-19 16:40:36 +0200

    Items in list

--- week-06/day-03/ParkingLot2/src/main/java/Lot.java
+++ week-06/day-03/ParkingLot2/src/main/java/Lot.java
@@ -1,10 +1,22 @@
+import java.util.ArrayList;
+
+//        Count and Print the number of same vehicles for each type
+//        Count and Print the number of same vehicles for each color
+//        Print the most frequently occurring vehicle
+
 public class Lot {
     public static void main(String[] args) {
-       RandomEnum<Car> rCar = new RandomEnum<Car>(Car.class);
-       RandomEnum<Color> rColor = new RandomEnum<Color>(Color.class);
-       System.out.println(rCar.randomValue());
-       System.out.println(rColor.randomValue());
+        RandomEnum<Car> rCar = new RandomEnum<Car>(Car.class);
+        RandomEnum<Color> rColor = new RandomEnum<Color>(Color.class);
+        ArrayList<String> carList = new ArrayList<String>();
 
+        for (int i =1; i <= 256 ; i++) {
+            carList.add(rColor.randomValue() + "_" + rCar.randomValue());
+        }
+
+        for(String item : carList){
+            System.out.println(item);
+        }
 
 
     }
