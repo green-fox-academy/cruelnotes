bbf3a3fc43152298411e408cad4a2e8dc88be73e
cruelnotes <adam.sequence@gmail.com>
2017-10-31 11:17:33 +0100

    h2 init

---
+++ week-08/day-02/h2todo/src/main/java/com/spring/h2todo/Todo.java
@@ -0,0 +1,12 @@
+package com.spring.h2todo;
+
+public class Todo {
+
+    private int id;
+    private String title;
+    private boolean isUrgent = false;
+    private boolean isDone = false;
+
+
+
+}
---
+++ week-08/day-02/h2todo/src/main/java/com/spring/h2todo/TodoController.java
@@ -0,0 +1,12 @@
+package com.spring.h2todo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/todo")
+public class TodoController {
+
+
+
+}
