49d5adad88a5bbdc89e6ccc6c151e6dd7101e50a
cruelnotes <adam.sequence@gmail.com>
2017-11-08 16:15:09 +0100

    Optionel excersises initial commit

---
+++ week-09/day-03/optional/src/main/java/com/spring/optional/OptionalApplication.java
@@ -0,0 +1,12 @@
+package com.spring.optional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptionalApplication.class, args);
+	}
+}
