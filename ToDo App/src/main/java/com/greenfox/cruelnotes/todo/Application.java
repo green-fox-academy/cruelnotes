e023c50ae84e3e5812159099f7b1b789aa1f8408
cruelnotes <adam.sequence@gmail.com>
2017-11-09 15:01:59 +0100

    Template added

--- ToDo App/src/main/java/com/greenfox/cruelnotes/todo/Application.java
+++ ToDo App/src/main/java/com/greenfox/cruelnotes/todo/Application.java
@@ -1,5 +1,9 @@
 package com.greenfox.cruelnotes.todo;
 
+import com.greenfox.cruelnotes.todo.models.Todo;
+import com.greenfox.cruelnotes.todo.repositories.AssigneeRepo;
+import com.greenfox.cruelnotes.todo.repositories.TodoRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +11,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
+	@Autowired
+	TodoRepo todoRepo;
+
+	@Autowired
+    AssigneeRepo assigneeRepo;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		todoRepository.save(new Todo("I need to do this.", true, false));
+		todoRepo.save(new Todo("I need to do this.", true, false));
 	}
 }
--- ToDo App/src/main/java/com/greenfox/cruelnotes/todo/models/Assignee.java
+++ ToDo App/src/main/java/com/greenfox/cruelnotes/todo/models/Assignee.java
@@ -2,6 +2,7 @@ package com.greenfox.cruelnotes.todo.models;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
