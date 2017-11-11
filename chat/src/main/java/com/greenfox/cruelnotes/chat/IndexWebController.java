bc3eec6450a0f15ab225329486dc752307eed745
cruelnotes <adam.sequence@gmail.com>
2017-11-11 23:48:52 +0100

    Partnerdemo files

--- chat/src/main/java/com/greenfox/cruelnotes/chat/IndexWebController.java
+++ chat/src/main/java/com/greenfox/cruelnotes/chat/IndexWebController.java
@@ -1,4 +1,14 @@
 package com.greenfox.cruelnotes.chat;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexWebController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
 }
--- thymeleafdemo/src/main/java/com/spring/thymeleafdemo/PostController.java
+++ thymeleafdemo/src/main/java/com/spring/thymeleafdemo/PostController.java
@@ -26,17 +26,3 @@ public class PostController {
         return body;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-//pageTitle
-//post.body
